b0ec828eac937755cadf6c92d8cdf797074ad040
fauzzana <hafidaji93@gmail.com>
2026-03-18 09:24:31 +0700

    fix(start page): memindahkan halaman awal ke login

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { ComponentExample } from "@/components/component-example";
 import { CardDemo } from "@/components/auth/app-signin";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  return <ComponentExample />;
+  redirect("/signin");
 }
